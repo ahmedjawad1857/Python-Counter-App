0c0c4e258d5c8a7df2be1a8b3d6226f3b8968bdf
ahmadjawad1857 <ahmedjawad1857@gmail.com>
2024-02-25 15:54:58 +0500

    Counter App In Python

---
+++ python.py
@@ -0,0 +1,39 @@
+# Counter App
+
+import time
+
+
+class Counter:
+    def __init__(self)->None:
+        self.count = 0
+    def addCount(self)->None:
+        self.count += 1   
+    def removeCount(self)->None:
+        self.count -= 1
+    def resetCount(self)->None:
+         self.count = 0
+counter = Counter()         
+while True:
+    print("\n===== Counter Management System =====")
+    print(f"Current Value Of Counter:  {counter.count}")
+    print("1. increase Count")
+    print("2. decrease count")
+    if counter.count > 0 or counter.count < 0:
+        print("3. Reset count to zero")
+    print("4. Exit") 
+    choice = input("Enter Your Choice:  ") 
+    match choice:
+        case "1":
+            counter.addCount()
+            
+        case "2":
+            counter.removeCount()
+            
+        case "3":
+            counter.resetCount()
+            
+        case "4":
+            break
+        case _:
+            print("Invalid Choice")
+    
